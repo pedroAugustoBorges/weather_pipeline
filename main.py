61f7a1f8d903340458ffcff4f068ea7eb7311de4
Pedro Augusto de Melo Borges <pedroameloborges@gmail.com>
2026-03-02 20:13:31 -0300

    Inital commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pipeline-weather!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/extract_data.py
@@ -0,0 +1,48 @@
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+import requests
+import json
+import logging
+
+
+logging.basicConfig(level = logging.INFO, format = '%(asctime)s - %(levelname)s  = %(message)s')
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+env_path = BASE_DIR / "config" / ".env"
+
+load_dotenv(env_path)
+
+api_key = os.getenv('API_KEY')
+
+print(api_key)
+
+url = f'https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo,BR&units=metrics&appid={api_key}'
+
+def extract_weather_data(url : str) -> list:
+    response = requests.get(url)
+    data = response.json()
+    
+    print("Status code:", response.status_code)
+    print("Response Text:", response.text)
+
+    
+    if response.status_code != 200:
+        logging.error("Erro in request")
+        
+        return []
+    
+    if not data:
+        logging.error("None data returned")
+        
+    output_path= 'data/waether_data.json'
+    output_dir = Path(output_path).parent
+    output_dir.mkdir(parents=True, exist_ok=True)
+    
+    with open(output_path, 'w') as f:
+        json.dump(data, f, indent=4)
+        
+    return data
+
+extract_weather_data(url)
