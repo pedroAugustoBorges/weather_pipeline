881ff9af2d5d1f9863a81e1cda0ffef40f19a96e
Pedro Augusto de Melo Borges <pedroameloborges@gmail.com>
2026-03-06 20:09:52 -0300

    Create function to normalize and drop columns

--- src/transform_data.py
+++ src/transform_data.py
@@ -26,4 +26,36 @@ def create_datafame (path_name):
 
 
 path_name = extract_path_from_file('data', 'weather_data.json') 
-print(create_datafame(path_name))
+df = (create_datafame(path_name))
+
+def normalize_weather_columns(df):
+    df_weather = pd.json_normalize(df['weather'].apply(lambda x : x[0]))
+    
+   
+    
+    df_weather = df_weather.rename(columns= {
+        "id" : "weather_id",
+        "main" : "weather_main",
+        "description" : "weather_description",
+        "icon" : 'weather_icon'
+    })
+    
+    df = pd.concat([df, df_weather], axis = 1)
+    
+    logging.info(f"\n Column 'weather' was normalized - {len(df.columns)} column")
+
+    return df
+
+df = normalize_weather_columns(df)
+
+
+
+def drop_column(df, columns_names):
+    logging.info(f"Deleting the {columns_names} from {df.columns}")
+    logging.info(f"Actual columns length from table: {len(df.columns)} ")
+    df = df.drop(columns = columns_names)
+    logging.info(f"Columns removed -  {len(df.columns)} remaining ")
+    
+    return df
+
+df = drop_column(df, [])
